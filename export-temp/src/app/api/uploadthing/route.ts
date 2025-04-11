6704d0c3b204a019b85d2140663713a20bf4b65c
annard24 <anna.dornier@gmail.com>
2025-04-11 04:37:34 -0500

    Delete export-temp directory

--- export-temp/src/app/api/uploadthing/route.ts
+++
@@ -1,7 +0,0 @@
-import { createNextRouteHandler } from "uploadthing/next";
-import { uploadRouter } from "./core";
-
-// Export routes for Next.js App Router
-export const { GET, POST } = createNextRouteHandler({
-  router: uploadRouter,
-});
